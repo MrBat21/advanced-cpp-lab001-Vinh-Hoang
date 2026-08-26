4231f98876712d516b192585afbee56a735b1f5c
MrBat21 <vinh.donhoang@gmail.com>
2026-08-25 22:17:29 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,8 @@
 #include "project/exercise.hpp"
 using namespace std;
 
-std::string get_greeting() {
-    return "Hello, Pextra Academy!";
-}
+
 int main() {
-    std::cout << get_greeting() << '\n';
-    cout << hello_message(); // LAB: UNCOMMENT THIS LINE
+    std::cout << hello_message() << '\n';
     return 0;
 }
